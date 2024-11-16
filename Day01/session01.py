73adabde9ce237c75cefa5edc98954d287dde908
shubham.pandey <pandeys2602@gmail.com>
2024-11-16 17:35:07 +0530

    created session01

---
+++ Day01/session01.py
@@ -0,0 +1,11 @@
+print("shubham")
+x = 10
+print(x)
+print("x is" + " equal to -", x)
+
+print(2+3+5.6)
+y=3
+print(y+2.6)
+
+print("shubham", "pandey", 34, 1.2, sep="||", end="\n")  # complete logic for print(value, separation, end /n-newline, /t-tab sapce)
+print("kanha", "pandey", 15, 1.2, sep="***")
