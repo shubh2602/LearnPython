662f551174723b57793d506982becdfa4f249859
shubham.pandey <pandeys2602@gmail.com>
2024-11-16 17:45:43 +0530

    basic calculator

--- Day01/session01.py
+++ Day01/session01.py
@@ -9,3 +9,25 @@ print(y+2.6)
 
 print("shubham", "pandey", 34, 1.2, sep="||", end="\n")  # complete logic for print(value, separation, end /n-newline, /t-tab sapce)
 print("kanha", "pandey", 15, 1.2, sep="***")
+
+x = input("enter the no.-")
+print(x+5)          #error - can only concatenate str (not "int") to str
+
+x = int(input("enter the no.-"))
+print(x + 5)  # pass
+
+# calculator
+x = int(input("enter value of x - "))
+y = int(input("enter value of y - "))
+print(x + y)
+print(x * y)
+print(x - y)
+print(x / y)
+
+#output
+# enter value of x - 10
+# enter value of y - 20
+# 30
+# 200
+# -10
+# 0.5
