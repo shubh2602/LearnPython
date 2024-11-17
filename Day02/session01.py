9d4612fc5eafebf12092754a7b91287e8629029c
shubham.pandey <pandeys2602@gmail.com>
2024-11-17 07:12:59 +0530

    table creation

---
+++ Day02/session01.py
@@ -0,0 +1,27 @@
+# import keyword
+#
+# print(keyword.kwlist)
+
+# out = ['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+# keywords can't be used as variables
+
+# age = 7
+# age = age + 1
+# print(age)
+# age = 45
+# print(age * 2, "is the product of 45*2")
+
+# Q - table of 2 with just print statement
+x=int(input("enter the number"))
+print(x, "* 1 = ", x*1)
+print(x, "* 2 = ", x*2)
+print(x, "* 3 = ", x*3)
+print(x, "* 4 = ", x*4)
+print(x, "* 5 = ", x*5)
+
+x=int(input("enter the number"))
+print(f"{x} * 1 = {x * 1}")
+print(f"{x} * 2 = {x * 2}")
+print(f"{x} * 3 = {x * 3}")
+print(f"{x} * 4 = {x * 4}")
+print(f"{x} * 5 = {x * 5}")
