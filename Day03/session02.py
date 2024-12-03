112d4678703c94dde4a49ecbeeaade86f93e72ec
shubham.pandey <pandeys2602@gmail.com>
2024-12-03 07:51:44 +0530

    python functions

--- Day03/session02.py
+++ Day03/session02.py
@@ -39,10 +39,10 @@ list1 = [1, 2, 3]
 list2 = [1, 2, 3]
 print(list1 is list2)  # Prints: False (coz they stored at different location)
 
-a=1
-b=0
-print(a and b)   # 0
-print(a or b)    # 1
+a = 1
+b = 0
+print(a and b)  # 0
+print(a or b)  # 1
 
 a = 2
 a += 5  # a=a+5
@@ -53,7 +53,7 @@ print(a)  # 4
 print(b)  # 1
 print(a > b)  # False a=2, b=3
 print(b >= a)  # true
-print(a==b) # false
+print(a == b)  # false
 
 # calculate area of circle
 rad = float(input("enter radius - "))
@@ -66,7 +66,7 @@ print(3.14 * rad * rad)
 # enter radius - 3.14
 # area of cirle is -  30.959144000000002
 
-#Create a program that takes two numbers as input and prints whether the first number is greater than, less than, or equal to the second number.
+# Create a program that takes two numbers as input and prints whether the first number is greater than, less than, or equal to the second number.
 a = int(input("enter 1st no. - "))
 b = int(input("enter 2st no. - "))
 if a > b:
@@ -83,8 +83,9 @@ print("x is greater" if x > y else "y is greater")
 
 # Develop a Python script that calculates the square and cube of a given number.
 x = int(input("enter no. - "))
-print("cube of a no. is - ", x**3)   #8
-print("square of a no. is - ", x**2)   #9
+print("cube of a no. is - ", x ** 3)  # 8
+print("square of a no. is - ", x ** 2)  # 9
+
 
 # output
 # enter no. - 3
---
+++ Day06/session01.py
@@ -0,0 +1,8 @@
+def area_of_circle(radius):
+    area = 3.14 * (pow(radius, 2))
+    print(area)
+
+
+# calculate area of circle
+rad = float(input("enter radius - "))
+area_of_circle(rad)
