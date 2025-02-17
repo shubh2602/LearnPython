c41b92a4bdb6cf5ab4c3d82d6e4287c583268e15
shubham.pandey <pandeys2602@gmail.com>
2025-02-17 10:25:05 +0530

    added new pattern

--- Practice/Loops_Conditions.py
+++ Practice/Loops_Conditions.py
@@ -91,3 +91,15 @@ for i in range(0, 9):
         for j in range(0, i + 1):
             print('*', end=' ')
         print()
+
+#         *
+#       * *
+#     * * *
+#   * * * *
+# * * * * *
+for i in range(1, 6):
+    for j in range(5, i, -1):
+        print(" ", end=' ')
+    for k in range(1, i+1, 1):
+        print("* ", end='')
+    print()
