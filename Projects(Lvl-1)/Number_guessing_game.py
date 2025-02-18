c3f0b1364d2f026a4502c42045aaf753ec5df32d
shubham.pandey <pandeys2602@gmail.com>
2025-02-18 10:05:37 +0530

    added new pattern

--- Projects(Lvl-1)/Number_guessing_game.py
+++ Projects(Lvl-1)/Number_guessing_game.py
@@ -9,7 +9,7 @@ guess_count = 0
 
 while guess_count < chances:
     guess_count += 1
-    my_guess=int(input("Please enter your guess - "))
+    my_guess = int(input("Please enter your guess - "))
     if my_guess == number_to_guess:
         print(f'The number is {number_to_guess} and you found it right !! in the {guess_count} attempt')
         break
